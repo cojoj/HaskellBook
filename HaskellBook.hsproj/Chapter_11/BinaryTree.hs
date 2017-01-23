5667cf776cea798ab3e77972016c555f85455085
Mateusz Zając <zajac.mateusz@icloud.com>
2017-01-23 14:42:34 +0100

    Convert tree to list

--- HaskellBook.hsproj/Chapter_11/BinaryTree.hs
+++ HaskellBook.hsproj/Chapter_11/BinaryTree.hs
@@ -22,3 +22,19 @@ testTree' :: BinaryTree Integer
 testTree' = Node (Node Leaf 3 Leaf) 1 (Node Leaf 4 Leaf)
 
 mapExpected = Node (Node Leaf 4 Leaf) 2 (Node Leaf 5 Leaf)
+
+-- To list
+
+testTree = Node (Node Leaf 1 Leaf) 2 (Node Leaf 3 Leaf)
+
+preorder :: BinaryTree a -> [a]
+preorder Leaf = []
+preorder (Node left a right) = [a] ++ preorder left ++ preorder right
+
+inorder :: BinaryTree a -> [a]
+inorder Leaf = []
+inorder (Node left a right) = preorder left ++ [a] ++ preorder right
+
+postorder :: BinaryTree a -> [a]
+postorder Leaf = []
+postorder (Node left a right) = preorder left ++ preorder right ++ [a]
