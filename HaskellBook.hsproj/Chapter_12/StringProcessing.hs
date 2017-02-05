21329d690054463ce7fb086b724394309ab0d843
Mateusz Zając <zajac.mateusz@icloud.com>
2017-02-05 18:12:26 +0100

    String processing for Chapter 12

--- HaskellBook.hsproj/Chapter_12/StringProcessing.hs
+++ HaskellBook.hsproj/Chapter_12/StringProcessing.hs
@@ -11,3 +11,23 @@ replaceThe :: String -> String
 replaceThe str = intercalate " " $ map athe $ fmap notThe $ words str
   where athe Nothing = "a"
         athe (Just x) = x
+        
+-- Exercise 2
+
+countTheBeforeVowel :: String -> Integer
+countTheBeforeVowel xs = matcher 0 $ words xs
+    where
+        isVowel x = x `elem` "aeiou"
+        matcher n (x:y:ys)
+            | x == "the" && isVowel (head y) = matcher (n + 1) (y:ys)
+            | otherwise = matcher n ys
+        matcher n _ = n
+        
+-- Exercise 3
+
+countVowels :: String -> Integer
+countVowels xs = foldl isVowel 0 xs
+    where 
+        isVowel n x
+            | x `elem` "aeiou"  = n + 1
+            | otherwise         = n
