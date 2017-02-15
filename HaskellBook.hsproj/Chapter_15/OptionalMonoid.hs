1c64dc7d90889fbf345b33a154dd723cb68250c9
Mateusz Zając <zajac.mateusz@icloud.com>
2017-02-15 14:46:44 +0100

    Chapter 15 first exercise

---
+++ HaskellBook.hsproj/Chapter_15/OptionalMonoid.hs
@@ -0,0 +1,12 @@
+module Chapter_15.OptionalMonoid where
+    
+import Data.Monoid
+
+data Optional a = Nada | Only a 
+                    deriving (Eq, Show)
+                    
+instance Monoid a => Monoid (Optional a) where
+    mempty = Nada
+    mappend Nada _ = Nada
+    mappend _ Nada = Nada
+    mappend (Only a) (Only b) = Only $ a <> b
