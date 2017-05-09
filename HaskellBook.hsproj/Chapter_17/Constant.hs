5ec8e26dc30c3aae46b3d2d11e37b8aeb63a87cc
Mateusz Zając <zajac.mateusz@icloud.com>
2017-05-09 09:09:33 +0200

    Chapter 17 Constant

---
+++ HaskellBook.hsproj/Chapter_17/Constant.hs
@@ -0,0 +1,12 @@
+module Chapter_17.Constant where
+    
+newtype Constant a b =
+  Constant { getConstant :: a }
+  deriving (Eq, Ord, Show)
+
+instance Functor (Constant a) where
+  fmap _ (Constant a) = Constant a
+  
+instance Monoid a => Applicative (Constant a) where
+  pure x = Constant x
+  (<*>) (Constant a) (Constant b) = Constant (a b)
