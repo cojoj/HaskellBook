7bd0d5d364a6c6f05c2001432a81a58b7bf5381b
Mateusz Zając <zajac.mateusz@icloud.com>
2017-05-09 09:04:55 +0200

    Chapter 17 Identity

---
+++ HaskellBook.hsproj/Chapter_17/Identity.hs
@@ -0,0 +1,10 @@
+module Chapter_17.Identity where
+    
+newtype Identity a = Identity a deriving (Eq, Ord, Show)
+
+instance Functor Identity where
+  fmap f (Identity a) = Identity (f a)
+
+instance Applicative Identity where
+  pure x = Identity x
+  (<*>) (Identity f) (Identity a) = Identity (f a)
